c1fa97aa7a1fa52238891fc5612cd41a71758725
Masaki Yatsu <yatsu@yatsu.info>
2022-12-18 20:32:58 +0900

    chore: Create request topic with 3 partitions

--- scripts/topics/create.sh
+++ scripts/topics/create.sh
@@ -5,13 +5,11 @@
 [ -f .env ] && source .env
 
 kafka-topics --create --topic "nf.request" --bootstrap-server ${KAFKA_SERVER} \
-    --replication-factor 1 --partitions 1 || true
+    --replication-factor 1 --partitions 3 || true
 
 kafka-topics --create --topic "nf.sse.response" --bootstrap-server ${KAFKA_SERVER} \
     --replication-factor 1 --partitions 1 || true
 
-kafka-topics --create --topic "nf.streaming.request" --bootstrap-server ${KAFKA_SERVER} \
-    --replication-factor 1 --partitions 1 || true
 kafka-topics --create --topic "nf.streaming.response.1" --bootstrap-server ${KAFKA_SERVER} \
     --replication-factor 1 --partitions 1 || true
 kafka-topics --create --topic "nf.streaming.response.2" --bootstrap-server ${KAFKA_SERVER} \
